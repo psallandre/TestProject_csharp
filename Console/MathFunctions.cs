4fc7668324f924517a27c7fa8594abd1445093d2
psallandreSurface <patrice@sallandre.com>
2013-07-13 08:44:30 +0200

    Add UnitTest project (MSTest) + Factorial test part 1

---
+++ Console/MathFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleProj
+{
+   public static class MathFunctions
+   {
+      public static int Factorial(int i) { 
+         if(i<0) throw new ArgumentOutOfRangeException();
+         return 0;
+      }
+   }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleProj;
+
+namespace UnitTestProject1
+{
+   [TestClass]
+   public class UnitTest1
+   {
+      [TestMethod]
+      [ExpectedException(typeof(ArgumentOutOfRangeException))]
+      public void FactorialShouldThrowIfNegatif() {
+         MathFunctions.Factorial(-1);
+      }
+
+      [TestMethod]
+      public void FactorialShouldbeOk(int param, int result) {
+         Assert.AreEqual(MathFunctions.Factorial(param), param);
+      }
+   }
+}
