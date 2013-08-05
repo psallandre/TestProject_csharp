e57aac68befd9d8fb7cc7c65dc090fc3a175d067
psallandreSurface <patrice@sallandre.com>
2013-08-05 08:48:36 +0200

    Create Tools project + Tools.Test project

---
+++ .UnitTest/Tools.Test/MathFunctionsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tools;
+
+namespace Tools.Test
+{
+   [TestClass]
+   public class MathFunctionsTest
+   {
+      [TestMethod]
+      [ExpectedException(typeof(ArgumentOutOfRangeException))]
+      public void FactorialShouldThrowIfNegatif() {
+         MathFunctions.Factorial(-1);
+      }
+
+      [TestMethod]
+      public void FactorialShouldbeOkFor() {
+         FactorialShouldbeOk(1, 0);
+         FactorialShouldbeOk(1, 1);
+         FactorialShouldbeOk(2, 2);
+         FactorialShouldbeOk(6, 3);
+         FactorialShouldbeOk(24, 4);
+      }
+
+      [TestMethod]
+      public void FactorialShouldbeOk(int expected, int param) {
+         Assert.AreEqual(expected, MathFunctions.Factorial(param));
+      }
+   }
+}
--- Tools/MathFunctions.cs
+++ Tools/MathFunctions.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ConsoleProj
+namespace Tools
 {
    public static class MathFunctions
    {
       public static int Factorial(int i) { 
          if(i<0) throw new ArgumentOutOfRangeException();
-         return 0;
+         if (i == 0) return 1;
+         if (i == 1) return 1;
+         return i*Factorial(i-1);
       }
    }
 }
